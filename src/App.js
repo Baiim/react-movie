0ec639c138a0993c3a881520d5b82f0423d72648
Sektiawan Bimo Prihpambudi <sektia7@gmail.com>
2022-07-22 13:55:24 +0700

    Update

--- src/App.js
+++ src/App.js
@@ -15,16 +15,16 @@ function App() {
     getMovies(FEATURED_API)
   }, [])
 
-  const getMovies = API => {
+  const getMovies = (API) => {
     fetch(API)
-      .then(res => res.json())
-      .then(data => {
+      .then((res) => res.json())
+      .then((data) => {
         console.log(data)
         setMovies(data.results)
       })
   }
 
-  const handleOnSubmit = e => {
+  const handleOnSubmit = (e) => {
     e.preventDefault()
     if (searchTerm) {
       getMovies(SEARCH_API + searchTerm)
@@ -33,7 +33,7 @@ function App() {
     }
   }
 
-  const handleOnChange = e => {
+  const handleOnChange = (e) => {
     setSearchTerm(e.target.value)
   }
   return (
@@ -51,9 +51,8 @@ function App() {
       </header>
       <div className="movie-container">
         {movies.length > 0 &&
-          movies.map(movie => <Movie key={movie.id} {...movie} />)}
+          movies.map((movie) => <Movie key={movie.id} {...movie} />)}
       </div>
-      )
     </>
   )
 }
--- src/components/Movie.js
+++ src/components/Movie.js
@@ -2,7 +2,7 @@ import React from "react"
 
 const IMG_API = "https://image.tmdb.org/t/p/w1280/"
 
-const setVoteClass = vote => {
+const setVoteClass = (vote) => {
   if (vote >= 8) {
     return "green"
   } else if (vote >= 6) {
@@ -17,7 +17,7 @@ const Movie = ({ title, poster_path, overview, vote_average }) => (
     <img src={IMG_API + poster_path} alt={title} />
     <div className="movie-info">
       <h3 className="title">{title}</h3>
-      <span className={"tag ${setVoteClass(vote_average)}"}>
+      <span className={`tag ${setVoteClass(vote_average)}`}>
         {vote_average}
       </span>
     </div>
